bf9eb2dd38a23fe596566587d22b6525b254159f
Kingeru 'Ake Worapot Bhilabutra <worapot.kingeru@gmail.com>
2021-06-25 00:45:19 +0700

    Hello Word

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FufuDev_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
